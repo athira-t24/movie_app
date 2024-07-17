fd43538e2d8d86f1f5aa7f9a956be1773268185e
ATHIRA <athira86sree@gmail.com>
2024-07-17 13:54:45 +0530

    trial

--- src/Task2/Detail.jsx
+++ src/Task2/Detail.jsx
@@ -18,11 +18,11 @@ const Detail = () => {
     
     
   return (
-    <div style={{ backgroundColor:"black", color:"white", display:"flex"}}>
+    <div style={{ backgroundColor:"black", color:"white", }}>
     
-    <img src={imageUrl+test.details.backdrop_path} alt=""  style={{height:"500px", width:"500px"}}/>
+    <img src={imageUrl+test.details.backdrop_path} alt=""  style={{height:"500px", width:"500px" , marginTop:"50px", marginBottom:"25px"}}/>
     <h2>Name:"{test.details.original_title||test.details.name}"</h2>
-    <h3>{test.details.overview}</h3>
+    <h3 style={{width:"75%", margin:"auto"}}>{test.details.overview}</h3>
    <h3> Rating:<Rating name="half-rating-read" defaultValue={4} precision={4} readOnly />
    </h3>    <h3>Release date:{test.details.release_date}</h3>
     <h3>Language:{test.details.original_language}</h3>
